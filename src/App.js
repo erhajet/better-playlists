e5941cce1f04c11d54cd46aed4ce5ef907f154f9
Rhesa Utomo <venderblitz@gmail.com>
2018-02-08 17:42:52 +0700

    body bg-color placement in App.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-let textColorDefault = '#f2f2f2';
+/*https://erhajet-better-playlists.herokuapp.com/*/
+
 let defaultStyle = {
-  color: textColorDefault
+  color: '#f2f2f2'
 }
-document.body.style = 'background: #404040;';
 
 class Aggregate extends Component {
   render() {
@@ -51,7 +50,7 @@ class App extends Component {
   render() {
     return (
       <div style={defaultStyle} className="App">
-      <h1>Title</h1>
+      <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
       <Aggregate/>
       <Aggregate/>
       <Filter/>
